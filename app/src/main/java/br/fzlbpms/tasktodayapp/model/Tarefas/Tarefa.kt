56e266ce1895adbc429a04eb4d124b6355acffed
MariaWerlang <maria.werlang@etec.sp.gov.br>
2023-06-17 11:24:19 -0300

    Teste 1

---
+++ app/src/main/java/br/fzlbpms/tasktodayapp/model/Tarefas/Tarefa.kt
@@ -0,0 +1,13 @@
+package br.fzlbpms.tasktodayapp.model.Tarefas
+
+import java.util.Date
+
+class Tarefa (nome: String, detalhes: String?, createDate: Date, pzoFinal: Date) {
+
+    /*var status = "pendente"
+        get() {field}
+        set(value) {
+            field = value
+        }*/
+
+}
